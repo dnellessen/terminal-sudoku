80f48b8557c00538fe6dd47199dc21c14d668f5f
Daniel <danielnlsn.ofcl@gmail.com>
2022-07-10 22:51:24 +0200

    cleaned up and added functionality to move cursor and add numbers on board

--- window.py
+++ window.py
@@ -6,23 +6,71 @@ class Window:
     rows, cols = 0, 0
     cursor_x, cursor_y = 0, 0
 
+    def __init__(self):
+        self.board = WindowBoard()
+
     def main(self, win):
         curses.start_color()
         curses.use_default_colors()
 
-        self.rows, self.cols = win.getmaxyx()
+        Window.rows, Window.cols = win.getmaxyx()
 
         win.clear()
+        self.board.draw(win)
 
-        self.draw_board(win)
-        win.move(self.cursor_x, self.cursor_y)
+        Window.cursor_x, Window.cursor_y = self.board.cursors[0][0]
+        win.move(Window.cursor_x, Window.cursor_y)
 
-        win.refresh()
-        win.getch()
+        key = 0
+        brow, bcol = 0, 0
 
-    def draw_board(self, win):
-        center_x = self.cols // 2
-        center_y = self.rows // 2
+        while key != 'q':
+            key = win.getkey()
+
+            if key == 'KEY_LEFT':
+                bcol -= 1
+            elif key == 'KEY_RIGHT':
+                bcol += 1
+            elif key == 'KEY_UP':
+                brow -= 1
+            elif key == 'KEY_DOWN':
+                brow += 1
+
+            if bcol < 0:
+                bcol = 9-1
+            elif bcol > 9-1:
+                bcol = 0
+
+            if brow < 0:
+                brow = 9-1
+            elif brow > 9-1:
+                brow = 0
+
+            try:
+                key = int(key)
+                win.addstr(str(key))
+            except ValueError:
+                pass
+            finally:
+                Window.cursor_x, Window.cursor_y = self.board.cursors[brow][bcol]
+                win.move(Window.cursor_x, Window.cursor_y)
+                win.refresh()
+
+    def start(self):
+        curses.initscr()
+        curses.wrapper(self.main)
+
+    
+class WindowBoard:
+    def __init__(self):
+        self.cursors = [[() * 9] * 9 for _ in range(9)]
+
+    def add_cursor(self, brow, bcol, x, y):
+        self.cursors[brow][bcol] = (x, y)
+
+    def draw(self, win):
+        center_x = Window.cols // 2
+        center_y = Window.rows // 2
 
         board_width = 44
         board_start_x = center_x - (board_width // 2)
@@ -34,17 +82,20 @@ class Window:
         upperleft_x = board_start_x
         lowerright_y = board_start_y + 2
         lowerright_x = board_start_x + 4
-        for numrows in range(9):
-            if numrows % 3 == 0 and numrows > 0:
+
+        for numrow in range(9):
+            if numrow % 3 == 0 and numrow > 0:
                     upperleft_y += 1
                     lowerright_y += 1
 
-            for numcols in range(9):
-                if numcols % 3 == 0 and numcols > 0:
+            for numcol in range(9):
+                if numcol % 3 == 0 and numcol > 0:
                     upperleft_x += 2
                     lowerright_x += 2
 
                 rectangle(win, upperleft_y, upperleft_x, lowerright_y, lowerright_x)
+                self.add_cursor(numrow, numcol, upperleft_y + 1, upperleft_x + 2)
+
                 upperleft_x += 4
                 lowerright_x += 4
 
@@ -60,9 +111,6 @@ class Window:
             board_start_y - 1 + board_height, 
             board_start_x - 2 + board_width,
         )
-
-    def start(self):
-        curses.wrapper(self.main)
 
     
 win = Window()
