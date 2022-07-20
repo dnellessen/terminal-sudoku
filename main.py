35688478f8380dbf9fba798a0a5687dca34e8f8c
Daniel <danielnlsn.ofcl@gmail.com>
2022-07-20 15:46:20 +0200

    added arguments

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 from window import Window
 from exceptions import WindowTooSmallError
 
+import sys
+
+
+difficulty = 'medium'
+for arg in sys.argv:
+    if arg.startswith('-'):
+        match arg:
+            case '-e' | '-easy':
+                difficulty = 'easy'
+            case '-m' | '-medium':
+                difficulty = 'medium'
+            case '-h' | '-hard':
+                difficulty = 'hard'
+
+
 win = Window()
+win.board.difficulty = difficulty
 
 try:
     win.start()
